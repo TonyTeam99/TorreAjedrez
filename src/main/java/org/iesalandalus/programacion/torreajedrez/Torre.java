8f2022b4a1745c3e3fd68079185092998f23802b
TonyTeam99 <anton@LAPTOP-HKK6DF4P>
2021-11-13 15:31:10 +0100

    Commit 12. Torre. Getters and Setters y excepciones.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/Torre.java
@@ -4,6 +4,26 @@ public class Torre {
 	private Color color;
 	private Posicion posicion;
 
+	private void setPosicion(Posicion posicion) {
+
+		this.posicion = posicion;
+	}
+
+	public Posicion getPosicion() {
+		return posicion;
+	}
+
+	private void setColor(Color color) {
+		if (color == null) {
+			throw new NullPointerException("ERROR: No se puede asignar un color nulo.");
+		}
+		this.color = color;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
 	public Torre() {
 		posicion = new Posicion(8, 'h');
 		setColor(Color.NEGRO);
