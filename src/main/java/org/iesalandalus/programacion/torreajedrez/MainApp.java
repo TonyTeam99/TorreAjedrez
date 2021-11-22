5aaa6e5a779f3eda9f969e0f2fb56a3d66fbcd4b
TonyTeam99 <anton@LAPTOP-HKK6DF4P>
2021-11-22 19:04:56 +0100

    COMMIT 19. Mostrar Menú.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -5,6 +5,9 @@ public class MainApp {
 
 	public static void main(String[] args) {
 		System.out.println("kk");
+		Torre polla = new Torre();
+		System.out.println(polla.toString());
+
 	}
 
 	// Método mostrar Torre
@@ -14,6 +17,16 @@ public class MainApp {
 		} catch (NullPointerException t) {
 			System.out.println("ERROR: La torre no se ha creado.");
 		}
+	}
+
+	// Método mostrar Menú
+	private static void mostrarMenu() {
+		System.out.println(" ");
+		System.out.println("1. Crear una torre por defecto.");
+		System.out.println("2. Crear una torre eligiendo su color.");
+		System.out.println("3. Crear una torre eligiendo su color y posición inicial.");
+		System.out.println("4. Mover la torre.");
+		System.out.println("5. Salir.");
 	}
 
 }
