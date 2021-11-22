dfe180654851ebed3e1d68a1ae2949efc17f67d0
TonyTeam99 <anton@LAPTOP-HKK6DF4P>
2021-11-22 18:56:57 +0100

    COMMIT 18. MostrarTorre.

--- src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/torreajedrez/MainApp.java
@@ -1,9 +1,19 @@
 package org.iesalandalus.programacion.torreajedrez;
 
 public class MainApp {
+	private static Torre torre;
 
 	public static void main(String[] args) {
 		System.out.println("kk");
+	}
+
+	// Método mostrar Torre
+	private static void mostrarTorre() {
+		try {
+			System.out.println(torre.toString());
+		} catch (NullPointerException t) {
+			System.out.println("ERROR: La torre no se ha creado.");
+		}
 	}
 
 }
